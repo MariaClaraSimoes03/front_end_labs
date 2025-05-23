7de5a2ec9e5500fa4b7e420d79db8dad002369a5
MariaClaraSimoes03 <mariaclarasimoes03@gmail.com>
2025-05-22 22:21:02 -0300

    Virtual Phone parte2

---
+++ virtual.phone/main.js
@@ -0,0 +1,25 @@
+const display =document.querySelector('input[type=tel]');
+
+const teclas=document.querySelectorAll('input[type=button]');
+
+const limpar = document.querySelector('.icon');
+
+limpar.onclick = () => {
+  const opcao = confirm( 'Deseja mesmo limpar?');
+  if (opcao){
+    display.value= "";
+}
+
+  }
+  
+function valorTeclaToDisplay(i){
+  display.value += teclas[i].value;
+}
+
+for(let i = 0; i < teclas.length; i++){
+  console.log(teclas[i].value);
+
+  teclas[i].onclick =() => {
+  valorTeclaToDisplay(i);}
+}
+
